d0ae160555f9030f20365e9931c64690d4f7ea23
luisuriarte <luisuriarte@gmail.com>
2024-12-28 23:02:49 -0300

    modif

--- beds/assign/load_beds.php
+++ beds/assign/load_beds.php
@@ -51,11 +51,11 @@ switch ($bedAction) {
 // Obtener las camas del cuarto seleccionado
 $bedPatients = getBedsPatientsData($roomId);
 
-echo '<pre>'; // Opcional: Formatea la salida para facilitar la lectura
-var_dump($beds);
-echo '</pre>';
-echo 'Bed Action: ' . $bedAction;
-?>
+//echo '<pre>'; // Opcional: Formatea la salida para facilitar la lectura
+//var_dump($beds);
+//echo '</pre>';
+//echo 'Bed Action: ' . $bedAction;
+//?>
 
 <!DOCTYPE html>
 <html lang="es">
