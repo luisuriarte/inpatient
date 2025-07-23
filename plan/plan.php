130f68a4ab4d386e5a322cf6f68dcd69dc9e0958
luisuriarte <luisuriarte@gmail.com>
2025-07-23 18:52:24 -0300

    Correcciones

--- plan/plan.php
+++ plan/plan.php
@@ -16,14 +16,6 @@ $title_patient_age = $patient_age;
 $title_patient_sex = $patient_sex;
 $title_insurance_name = $insurance_name;
 
-// Verificar si se ha seleccionado un paciente
-//if (empty($patient_id) || empty($patient_name)) {
-//    echo "<script>
-//        alert('" . xla('First you must choose a patient') . "');
-//        window.location.href = '../../../interface/main/finder/dynamic_finder.php';
-//    </script>";
-//    exit;
-//}
 $backgroundPatientCard = "#f6f9bc";
 ?>
 
